906175fc46d50cb7903d579cf55453381e5dffaa
kyoonge <96893605+kyoonge@users.noreply.github.com>
2023-04-21 01:02:34 +0900

    Destroy Tile

--- Assets/Scripts/HexTile.cs
+++ Assets/Scripts/HexTile.cs
@@ -9,6 +9,7 @@ public class HexTile : MonoBehaviour
     private bool isPushed = false;
     [SerializeField] private Material mat;
 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,18 +17,16 @@ public class HexTile : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
-        Debug.Log("trigger");
         if (other.gameObject.tag.Equals("Player") && !isPushed)
         {
             isPushed = true;
             transform.position = Vector3.MoveTowards(transform.position, new Vector3(0, -0.7f, 0), _pushVelocity);
             gameObject.GetComponent<MeshRenderer>().material = mat;
-            Invoke("ResetPosition", 1.5f);
+            Invoke("ResetDestroy", 1.5f);
         }
     }
 
-
-    private void ResetPosition()
+    private void ResetDestroy()
     {
         transform.Translate(new Vector3(0, +0.1f, 0));
         Destroy(this.gameObject, _destroyDelay);
